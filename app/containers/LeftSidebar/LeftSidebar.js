10f07e04778ce6ecb56003158066452314314783
Dewey Nguyen <dewey@rethinkworkflow.com>
2018-03-16 12:23:42 -0500

    added build number on the live app

--- app/containers/LeftSidebar/LeftSidebar.js
+++ app/containers/LeftSidebar/LeftSidebar.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { func, bool } from 'prop-types';
+import { func, bool, string, number, oneOfType } from 'prop-types';
 import MenuItem from 'material-ui/MenuItem';
 import Drawer from 'material-ui/Drawer';
+import { grey500 } from 'material-ui/styles/colors';
 
-const LeftSidebar = ({ toggleLeftSidebar, isLeftSidebarOpened }) => (
+const LeftSidebar = ({
+  toggleLeftSidebar, isLeftSidebarOpened, version, buildNumber, commit,
+}) => (
   <Drawer
     docked={false}
     width={200}
@@ -11,12 +14,32 @@ const LeftSidebar = ({ toggleLeftSidebar, isLeftSidebarOpened }) => (
     onRequestChange={toggleLeftSidebar}
   >
     <MenuItem onClick={toggleLeftSidebar}>Menu Item</MenuItem>
+    <div
+      className="footer"
+      style={{
+        position: 'absolute',
+        bottom: 0,
+        textAlign: 'center',
+        width: '100%',
+      }}
+    >
+      <code
+        style={{
+          color: grey500,
+        }}
+      >
+        {version}.{buildNumber}, #{commit}
+      </code>
+    </div>
   </Drawer>
 );
 
 LeftSidebar.propTypes = {
   toggleLeftSidebar: func.isRequired,
   isLeftSidebarOpened: bool.isRequired,
+  version: string.isRequired,
+  buildNumber: oneOfType([string, number]).isRequired,
+  commit: oneOfType([string, number]).isRequired,
 };
 
 export default LeftSidebar;
