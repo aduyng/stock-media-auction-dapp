bdeb12cb97d36a426ffdf4d567bab9f5ed6f7fac
Dewey Nguyen <dewey@rethinkworkflow.com>
2018-03-16 10:22:54 -0500

    removed home

--- app/containers/home/index.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux';
-import mapStateToProps from './selectors/mapStateToProps';
-import mapDispatchToProps from './selectors/mapDispatchToProps';
-import Component from './Home';
-
-export default connect(mapStateToProps, mapDispatchToProps)(Component);
