24135d727ca6b1d3e60a828a67d1e18ce9e0c506
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-16 17:28:20 +0000

    Deploy aduyng/stock-media-auction-dapp to github.com/aduyng/stock-media-auction-dapp.git:gh-pages

--- app/containers/LeftSidebar/selectors/mapStateToProps.js
+++ app/containers/LeftSidebar/selectors/mapStateToProps.js
@@ -1,3 +1,6 @@
 export default state => ({
   isLeftSidebarOpened: state.leftSidebar.isOpened,
+  commit: process.env.commit,
+  buildNumber: process.env.buildNumber,
+  version: process.env.version,
 });
